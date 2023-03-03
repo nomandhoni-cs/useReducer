89109b0db3b40354ac076a8512a053c35130356f
nomandhoni-cs <nomandhoni@yahoo.com>
2023-03-04 00:57:01 +0600

    Setting up use reducer

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import { useState } from "react";
+import { useReducer } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
 
 function App() {
   const [count, setCount] = useState(0);
+  const [state, dispatch] = useReducer(reducer, {age: 10});
+function reducer (state:any, action:any) {
+    switch (action.type) {
+        case 'increment':
+            return {age: state.age + 1};
+        case 'decrement':
+            return {age: state.age - 1};
+        default:
+            throw new Error();
+    }
+}
 
+const handleIncrease = () => {
+    dispatch({type: 'increment'});
+}
   return (
     <div className="App">
       <div>
@@ -16,6 +31,10 @@ function App() {
         </a>
       </div>
       <h1>useReducer Practice</h1>
+      <h1>
+        Age is {state.age}
+      </h1>
+      <button onClick={handleIncrease}>Increase Age</button>
       <div className="card">
         <h1>{count}</h1>
         <button onClick={() => setCount((count) => count + 1)}>
