bda4c316326596924f67308a79eb6ddfc336102a
nomandhoni-cs <nomandhoni@yahoo.com>
2023-03-08 00:36:58 +0600

    testing out lintign

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { useState } from "react";
-import { useReducer } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
+import { useState } from "react"
+import { useReducer } from "react"
+import reactLogo from "./assets/react.svg"
+import "./App.css"
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [state, dispatch] = useReducer(reducer, {age: 10});
-function reducer (state:any, action:any) {
+  const [count, setCount] = useState(0)
+  const [state, dispatch] = useReducer(reducer, { age: 10 })
+  function reducer(state: any, action: any) {
     switch (action.type) {
-        case 'increment':
-            return {age: state.age + 1};
-        case 'decrement':
-            return {age: state.age - 1};
-        default:
-            throw new Error();
+      case "increment":
+        return { age: state.age + 1 }
+      case "decrement":
+        return { age: state.age - 1 }
+      default:
+        throw new Error()
     }
-}
+  }
 
-const handleIncrease = () => {
-    dispatch({type: 'increment'});
-}
+  const handleIncrease = () => {
+    dispatch({ type: "increment" })
+  }
   return (
     <div className="App">
       <div>
@@ -31,15 +31,11 @@ const handleIncrease = () => {
         </a>
       </div>
       <h1>useReducer Practice</h1>
-      <h1>
-        Age is {state.age}
-      </h1>
+      <h1>Age is {state.age}</h1>
       <button onClick={handleIncrease}>Increase Age</button>
       <div className="card">
         <h1>{count}</h1>
-        <button onClick={() => setCount((count) => count + 1)}>
-          Increase
-        </button>
+        <button onClick={() => setCount((count) => count + 1)}>Increase</button>
         <br />
         <br />
         <button onClick={() => setCount((count) => count - 1)}>Decrease</button>
@@ -47,9 +43,10 @@ const handleIncrease = () => {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
+      {count ? <h1>Count is {count}</h1> : 0}
       <p className="read-the-docs">Noman Dhoni</p>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
