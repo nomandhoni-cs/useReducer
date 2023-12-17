d1afc99813aec8c9b02447b55771d7a1f4c9eee2
nomandhoni-cs <nomandhoni@yahoo.com>
2023-12-17 21:59:13 +0600

    Reading from the docs

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,4 +1,5 @@
 import { useReducer } from "react"
+import Controller from "./Controller"
 function reducer(state: any, action: any) {
   switch (action.type) {
     case "INCREMENT_AGE": {
@@ -7,8 +8,24 @@ function reducer(state: any, action: any) {
         age: state.age + 1,
       }
     }
+    case "DECREMENT_AGE": {
+      return {
+        name: state.name,
+        age: state.age - 1,
+      }
+    }
+    case "RESET_AGE": {
+      return {
+        name: state.name,
+        age: 0,
+      }
+    }
+    default:
+      return {
+        name: state.name,
+        state: state.age,
+      }
   }
-  throw Error("Unknown Action")
 }
 const Form = () => {
   const [state, dispatch] = useReducer(reducer, {
@@ -18,11 +35,20 @@ const Form = () => {
   const handleAgeIncrement = () => {
     dispatch({ type: "INCREMENT_AGE" })
   }
+  const handleAgeDecrement = () => {
+    dispatch({ type: "DECREMENT_AGE" })
+  }
+  const handleAgeReset = () => {
+    dispatch({ type: "RESET_AGE" })
+  }
   return (
     <>
-      <h1>Form</h1>
-      {state.age}
+      <h1>
+        {state.age} of {state.name} years
+      </h1>
       <button onClick={handleAgeIncrement}>Increment</button>
+      <button onClick={handleAgeDecrement}>Decrement</button>
+      <button onClick={handleAgeReset}>Reset</button>
     </>
   )
 }
